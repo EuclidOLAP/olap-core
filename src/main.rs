6c95e60611add00487dfe803a05dc23abd7af1d3
Fir <czg@euclidolap.com>
2024-12-17 10:56:51 +0000

    Add gRPC service interface for OLAP operations
    - Updated build.rs to include necessary gRPC dependencies.
    - Modified proto/euclidolap.proto to define OLAP service and operations.
    - Implemented gRPC server logic in src/main.rs to handle OLAP requests.
    - Added tests/olap-api-clients.bat to test the gRPC API client.
    
    These changes allow olap-core to expose a gRPC service for OLAP operations.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,60 @@
-// use std::{thread, time};
-use grpc_service::greeter_server::{Greeter, GreeterServer};
-use grpc_service::{ClientRequest, ServerResponse};
+use euclidolap::olap_api_server::{OlapApi, OlapApiServer};
+use euclidolap::{OlapRequest, OlapResponse, Row};
 use tonic::{transport::Server, Request, Response, Status};
 
-// // 自动生成的 gRPC 模块
-// pub mod grpc_service {
-//     tonic::include_proto!("grpc_service"); // 包名要与 .proto 文件中的 package 名字匹配
-// }
-
-mod grpc_service {
-    // 引用生成的模块文件 src/grpc/grpc_service.rs
-    include!("grpc/grpc_service.rs");
+mod euclidolap {
+    include!("grpc/euclidolap.rs");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct EuclidOLAPService {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
+impl OlapApi for EuclidOLAPService {
+    async fn execute_operation(
         &self,
-        request: Request<ClientRequest>, // 从客户端接收的请求
-    ) -> Result<Response<ServerResponse>, Status> {
+        request: Request<OlapRequest>, // 从客户端接收的请求
+    ) -> Result<Response<OlapResponse>, Status> {
         println!("Received request: {:?}", request);
 
-        // 获取请求中的数据
-        let client_message = request.into_inner().message;
+        // 从请求中解析操作类型和语句
+        let olap_request = request.into_inner();
+        let operation_type = olap_request.operation_type;
+        let statement = olap_request.statement;
 
-        // 创建响应
-        let reply = grpc_service::ServerResponse {
-            reply: format!("Hello, {}!", client_message),
+        println!(
+            "Operation Type: {}, Statement: {}",
+            operation_type, statement
+        );
+
+        // 伪造一个响应，返回结果
+        let response = OlapResponse {
+            rows: vec![
+                Row {
+                    columns: vec![
+                        "[ cell: R 0 C 0 ]".to_string(),
+                        "[ cell: R 0 C 1 ]".to_string(),
+                        "[ cell: R 0 C 2 ]".to_string(),
+                    ],
+                },
+                Row {
+                    columns: vec![
+                        "[ cell: R 1 C 0 ]".to_string(),
+                        "[ cell: R 1 C 1 ]".to_string(),
+                        "[ cell: R 1 C 2 ]".to_string(),
+                    ],
+                },
+                Row {
+                    columns: vec![
+                        "AAAsssDDD".to_string(),
+                        "111222333".to_string(),
+                        "@@@###$$$".to_string(),
+                    ],
+                },
+            ],
         };
 
-        // 返回响应
-        Ok(Response::new(reply))
+        Ok(Response::new(response))
     }
 }
 
@@ -44,13 +65,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50052".parse().unwrap();
 
     // 创建 Greeter 服务实例
-    let greeter = MyGreeter::default();
+    let olap_api_server = EuclidOLAPService::default();
 
     println!(">>> EuclidOLAP Server is listening on {} <<<", addr);
 
     // 启动 gRPC 服务端
     Server::builder()
-        .add_service(GreeterServer::new(greeter)) // 添加 Greeter 服务
+        .add_service(OlapApiServer::new(olap_api_server)) // 添加 Greeter 服务
         .serve(addr) // 启动服务端
         .await?;
 
