883fea1e9a89e253d6e0c0c0998fe10441fb8a33
Fir <czg@euclidolap.com>
2024-12-29 03:04:44 +0000

    a demo about `logos + lalrpop + ast`

--- build.rs
+++ build.rs
@@ -1,33 +1,3 @@
 fn main() {
-    let service_file = "proto/service.proto";
-    let euclidolap_file = "proto/euclidolap.proto";
-    let out_dir = "src/grpc";
-
-    // 编译proto/service.proto
-    if let Err(e) = tonic_build::configure()
-        .out_dir(out_dir)
-        .compile(&[service_file], &["proto"])
-    {
-        eprintln!("Failed to compile {}, the error is: {}", service_file, e);
-        std::process::exit(1);
-    }
-
-    // 编译proto/euclidolap.proto
-    if let Err(e) = tonic_build::configure()
-        .out_dir(out_dir)
-        .compile(&[euclidolap_file], &["proto"])
-    {
-        eprintln!("Failed to compile {}, the error is: {}", euclidolap_file, e);
-        std::process::exit(1);
-    }
-
-    // 文件更改监控
-    println!("cargo:rerun-if-changed={}", service_file);
-    println!("cargo:rerun-if-changed={}", euclidolap_file);
-
-    // // genarate mdx parser code
-    // lalrpop::process_root().unwrap_or_else(|e| {
-    //     eprintln!("LALRPOP code generation failed with error: {}", e);
-    //     std::process::exit(1);
-    // });
+    lalrpop::process_src().unwrap();
 }
--- src/mdx_parser/mod.rs
+++
@@ -1,8 +0,0 @@
-// mdx_parser/mod.rs
-
-// 引入子模块
-pub mod lexer;  // 词法解析相关代码
-// pub mod parser; // 语法解析相关代码
-// pub mod ast;    // 抽象语法树定义
-
-// 可以在这里定义一些公共函数或类型，供外部使用
