63ec089f702859c4afa7f08711cd09e2d9e64c1b
Fir <czg@euclidolap.com>
2025-01-05 02:54:54 +0000

    this calculator 2 example was finished.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+// lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+lalrpop_mod!(pub calculator2);
 
 #[test]
 fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    // assert!(calculator1::TermParser::new().parse("22").is_ok());
+    // assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    // assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    // assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    assert!(calculator2::TermParser::new().parse(">22@").is_ok());
+    assert!(calculator2::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator2::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator2::TermParser::new().parse("((22)").is_err());
+    println!("calculator2 test passed .....................");
 }
 
 fn main() {
