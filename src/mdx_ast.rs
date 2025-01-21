c06eb898001aeb691f794793bf2a350af2faf092
Fir <czg@euclidolap.com>
2025-01-21 11:27:22 +0000

    - Blocked the async method call to `ast_selstat.gen_md_context()` in `src/main.rs`. - Added `#[derive(Debug)]` to the `MultiDimensionalContext` struct in `src/mdd/mod.rs`. - Called olapmeta gRPC service to query Cube instances by GID or name in `src/mdx_ast.rs`. - Updated example MDX in `src/mdx_statements.rs`.

--- src/mdx_ast.rs
+++ src/mdx_ast.rs
@@ -1,16 +1,13 @@
 use crate::mdd;
+use crate::olapmeta_grpc_client::GrpcClient;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ExtMDXStatement {
-    Querying {
-        basic_cube: AstCube,
-    },
+    Querying { basic_cube: AstCube },
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct AstCube {
-
-}
+pub struct AstCube {}
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct AstSeg {
@@ -35,10 +32,7 @@ pub enum AstSet {
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum AstAxis {
-    Def{
-        ast_set: AstSet,
-        pos: u64
-    },
+    Def { ast_set: AstSet, pos: u64 },
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -49,82 +43,126 @@ pub struct AstSelectionStatement {
 }
 
 impl AstSelectionStatement {
-
-
-    pub fn gen_md_context(&self) -> mdd::MultiDimensionalContext {
-
-        // 获得真正Cube实例
-        // 通过cube gid或cube name获得cube实例
+    pub async fn gen_md_context(&self) -> mdd::MultiDimensionalContext {
+        // 获取真正的 Cube 实例
         let cube_pro = &self.cube;
         let ast_seg_opt = cube_pro.get(0);
 
-        let cube: mdd::Cube;
+        // 初始化默认 Cube
+        let mut cube = mdd::Cube {
+            gid: 0,
+            name: String::from(">>> There is no cube <<<"),
+        };
 
-        match ast_seg_opt {
-            Some(ast_seg) => {
-                let gid_opt = ast_seg.gid;
-                match gid_opt {
-                    Some(gid) => {
-                        println!("CCDFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF >>> gid >>>: {}", gid);
-                        cube = mdd::Cube {
-                            gid: 3000000000001,
-                            name: String::from("Caa"),
-                        }
-                    },
-                    None => {
-                        let seg_str_opt = &ast_seg.seg_str;
-                        match seg_str_opt {
-                            Some(seg_str) => {
-                                println!("CCDFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF >>> seg_str >>>: {}", seg_str);
-                                cube = mdd::Cube {
-                                    gid: 3000000000002,
-                                    name: String::from("Css"),
-                                }
-                            },
-                            None => {
-                                panic!("In method AstSelectionStatement::gen_md_context(): cube seg_str is empty!");
-                            }
-                        }
-                    }
-                }
-            },
-            None => {
-                panic!("In method AstSelectionStatement::gen_md_context(): cube is empty!");
-            }
+        // 创建 gRPC 客户端
+        let mut grpc_cli = GrpcClient::new("http://192.168.66.51:50051".to_string())
+            .await
+            .expect("Failed to create client");
+
+        // 如果没有 ast_seg，直接 panic
+        let ast_seg = match ast_seg_opt {
+            Some(ast_seg) => ast_seg,
+            None => panic!("In method AstSelectionStatement::gen_md_context(): cube is empty!"),
+        };
+
+        // 通过 gid 或 seg_str 查询 Cube
+        let gid_opt = ast_seg.gid;
+
+        if let Some(gid) = gid_opt {
+            println!("CCD >>> gid: {}", gid);
+            cube = self.fetch_cube_by_gid(&mut grpc_cli, gid).await;
+        } else {
+            let seg_str_opt = &ast_seg.seg_str;
+            let seg_str = seg_str_opt.as_ref().unwrap_or_else(|| {
+                panic!("In method AstSelectionStatement::gen_md_context(): cube seg_str is empty!")
+            });
+            println!("CCD >>> seg_str: {}", seg_str);
+            cube = self.fetch_cube_by_name(&mut grpc_cli, seg_str).await;
         }
 
-        println!("Cccc Cccc Cccc Cccc Cccc Cccc Cccc : {:#?}", cube);
+        println!("Final Cube: {:#?}", cube);
 
-        // todo 返回临时对象
-        mdd::MultiDimensionalContext {
-            cube: mdd::Cube {
-                gid: 50000000001,
-                name: String::from("test_cube"),
+        // 返回临时对象
+        mdd::MultiDimensionalContext { cube }
+    }
+
+    async fn fetch_cube_by_gid(&self, grpc_cli: &mut GrpcClient, gid: u64) -> mdd::Cube {
+        match grpc_cli.get_cube_by_gid(gid).await {
+            Ok(response) => {
+                println!("Received Cube by GID: {:?}", response);
+                response
+                    .cube_meta
+                    .map(|meta| mdd::Cube {
+                        gid: meta.gid,
+                        name: meta.name,
+                    })
+                    .unwrap_or_else(|| {
+                        println!("Error fetching Cube by GID: CubeMeta is None");
+                        mdd::Cube {
+                            gid: 0,
+                            name: String::from(">>> No cube found <<<"),
+                        }
+                    })
+            }
+            Err(e) => {
+                println!("Error fetching Cube by GID: {}", e);
+                mdd::Cube {
+                    gid: 0,
+                    name: String::from(">>> No cube found <<<"),
+                }
             }
         }
     }
 
+    async fn fetch_cube_by_name(&self, grpc_cli: &mut GrpcClient, name: &str) -> mdd::Cube {
+        match grpc_cli.get_cube_by_name(name.to_string()).await {
+            Ok(response) => {
+                println!("Received Cube by Name: {:?}", response);
+                response
+                    .cube_meta
+                    .map(|meta| mdd::Cube {
+                        gid: meta.gid,
+                        name: meta.name,
+                    })
+                    .unwrap_or_else(|| {
+                        println!("Error fetching Cube by Name: CubeMeta is None");
+                        mdd::Cube {
+                            gid: 0,
+                            name: String::from(">>> No cube found <<<"),
+                        }
+                    })
+            }
+            Err(e) => {
+                println!("Error fetching Cube by Name: {}", e);
+                mdd::Cube {
+                    gid: 0,
+                    name: String::from(">>> No cube found <<<"),
+                }
+            }
+        }
+    }
 
     pub fn build_axes(&self) -> Vec<mdd::Axis> {
-        println!("build_axes .. . ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        println!(
+            "build_axes .. . ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        );
 
         let axes_count = self.axes.len();
-        println!(">>> axes_count >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>: {}", axes_count);
+        println!(
+            ">>> axes_count >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>: {}",
+            axes_count
+        );
 
-        let mut axes:Vec<mdd::Axis> = Vec::with_capacity(axes_count);
+        let mut axes: Vec<mdd::Axis> = Vec::with_capacity(axes_count);
 
         for i in 0..axes_count {
-            let axis = mdd::Axis {
-                pos_num: i as u32,
-            };
+            let axis = mdd::Axis { pos_num: i as u32 };
             axes.push(axis);
         }
 
         axes
     }
 }
-
-
 
 // #[derive(Clone, Debug, PartialEq)]
 // pub enum Statement {
