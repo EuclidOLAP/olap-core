be8e1208a5b14c3e318b21b77faf295162c0c978
Fir <czg@euclidolap.com>
2025-01-22 08:36:43 +0000

    Cleaned up src/main.rs by removing cluttered test code.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
 use euclidolap::olap_api_server::{OlapApi, OlapApiServer};
 use euclidolap::{OlapRequest, OlapResponse, Row};
 use tonic::{transport::Server, Request, Response, Status};
-
-mod mdx_statements;
-use mdx_statements::{mdx_demo, mdx_demo_2_axposstr};
-
 mod olapmeta_grpc_client;
-use crate::olapmeta_grpc_client::GrpcClient;
-
-// mod mdx_parser;
-
 mod mdd;
 
 mod euclidolap {
@@ -24,16 +16,7 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub mdx_grammar);
 
-use crate::mdx_grammar::MdxStatementParser;
-// use crate::mdx_grammar::BlocksChainParser;
-use crate::mdx_grammar::AxesParser;
-use crate::mdx_grammar::AxisParser;
-use crate::mdx_grammar::SegParser;
-use crate::mdx_grammar::SegmentsParser;
-use crate::mdx_grammar::SegmentsWrapParser;
 use crate::mdx_grammar::SelectionMDXParser;
-use crate::mdx_grammar::SetWrapParser;
-use crate::mdx_grammar::TupleWrapParser;
 
 use crate::mdx_lexer::Lexer as MdxLexer;
 
@@ -93,44 +76,6 @@ impl OlapApi for EuclidOLAPService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // 创建 gRPC 客户端
-    let mut client = GrpcClient::new("http://192.168.66.51:50051".to_string())
-        .await
-        .expect("Failed to create client");
-
-    // 测试通过 GID 查询 Cube
-    match client.get_cube_by_gid(314).await {
-        Ok(response) => {
-            println!("Received Cube by GID: {:?}", response);
-        }
-        Err(e) => {
-            println!("Error fetching Cube by GID: {}", e);
-        }
-    }
-
-    // 测试通过名称查询 Cube
-    match client.get_cube_by_name("Tata.SSS".to_string()).await {
-        Ok(response) => {
-            println!("Received Cube by Name: {:?}", response);
-        }
-        Err(e) => {
-            println!("Error fetching Cube by Name: {}", e);
-        }
-    }
-    // ????????????????????????????????????????????????????????????????????
-
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    test_parsing_mdx_01();
-    test_parsing_mdx_02();
-    test_parsing_mdx_03();
-    test_parsing_mdx_04();
-    test_parsing_mdx_05();
-    test_parsing_mdx_06();
-    test_parsing_mdx_07();
-    test_parsing_mdx_08();
-    test_parsing_mdx_09();
-    // ????????????????????????????????????????????????????????????????????
 
     // 定义服务端监听地址
     // let addr = "127.0.0.1:50052".parse().unwrap();
@@ -182,202 +127,5 @@ async fn exe_md_query(ast_selstat: mdx_ast::AstSelectionStatement) -> () {
     /*
      * 构建真实的多维查询坐标轴
      */
-    let axes = ast_selstat.build_axes();
+    let _axes = ast_selstat.build_axes();
 }
-
-// #[test]
-// fn test_parsing_mdx_00() {
-//     handle_stat(String::from("MDX"), mdx_demo());
-
-//     let mut mdx2 = mdx_demo();
-//     mdx2.push(';');
-//     handle_stat(String::from("MDX"), mdx2);
-// }
-
-// #[test]
-fn test_parsing_mdx_01() {
-    let source_code = "Select --{}\n on from where";
-
-    let lexer = MdxLexer::new(source_code);
-    let parser = MdxStatementParser::new();
-    let ast = parser.parse(lexer).unwrap();
-
-    println!("MDX---------------------------------------{:?}", ast);
-}
-
-// #[test]
-fn test_parsing_mdx_02() {
-    let ast_node = SegParser::new()
-        .parse(MdxLexer::new("&0000000000000000000000000300000000321"))
-        .unwrap();
-    println!("MDX TEST 02 >>>>>>>>>>>>>>>>>>>>> {:?}", ast_node);
-    let ast_node = SegParser::new()
-        .parse(MdxLexer::new(
-            "&0000000000000000000000000000000000000000000000000000000000000000000300000000345[LA]",
-        ))
-        .unwrap();
-    println!("MDX TEST 02 >>>>>>>>>>>>>>>>>>>>> {:?}", ast_node);
-    let ast_node = SegParser::new()
-        .parse(MdxLexer::new("[[iPhone 16]] pro max]"))
-        .unwrap();
-    println!("MDX TEST 02 >>>>>>>>>>>>>>>>>>>>> {:?}", ast_node);
-}
-
-// Segments
-// #[test]
-fn test_parsing_mdx_03() {
-    println!(">>> mdx_test_03_segments >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    let ast_node = SegmentsParser::new()
-        .parse(MdxLexer::new("&100000123.&100000123.&100000123[>[[]]]]<]"))
-        .unwrap();
-    println!(">>> mdx_test_03_segments {:?}", ast_node);
-    let ast_node = SegmentsParser::new()
-        .parse(MdxLexer::new("[丰田].&100000123[兰德酷路泽].&100000123"))
-        .unwrap();
-    println!(">>> mdx_test_03_segments {:?}", ast_node);
-}
-
-// SegmentsWrapParser
-// #[test]
-fn test_parsing_mdx_04() {
-    println!(">>> 04 SegmentsWrapParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    let ast_node = SegmentsWrapParser::new()
-        .parse(MdxLexer::new("&100000123.&100000123.&100000123[>[[]]]]<]"))
-        .unwrap();
-    println!(">>> 04 SegmentsWrapParser {:?}", ast_node);
-}
-
-// TupleWrapParser
-// #[test]
-fn test_parsing_mdx_05() {
-    println!("\n\n");
-
-    println!(">>> 05 TupleWrapParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    let ast_node = TupleWrapParser::new()
-        .parse(MdxLexer::new(
-            "( [丰田].&100000123[兰德酷路泽].&100000123, [丰田].&100000123[兰德酷路泽], [丰田] )",
-        ))
-        .unwrap();
-    println!("{:?}", ast_node);
-
-    println!("\n\n");
-}
-
-// SetWrapParser
-// #[test]
-fn test_parsing_mdx_06() {
-    println!("\n\n");
-
-    println!(">>> 06 SetWrapParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    let mdx_fragment = "{
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004692[企业客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004692[企业客户] )
-}";
-    let ast_node = SetWrapParser::new()
-        .parse(MdxLexer::new(mdx_fragment))
-        .unwrap();
-    println!("{:?}", ast_node);
-
-    println!("\n\n");
-}
-
-// AxisParser
-// #[test]
-fn test_parsing_mdx_07() {
-    println!("\n\n");
-    println!(">>> 07 AxisParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-    let mdx_fragment = "{
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004692[企业客户] )
-} on 090";
-    let ast_node = AxisParser::new()
-        .parse(MdxLexer::new(mdx_fragment))
-        .unwrap();
-    println!("{:?}", ast_node);
-
-    println!("\n\n");
-}
-
-// AxesParser
-// #[test]
-fn test_parsing_mdx_08() {
-    println!("\n\n");
-    println!(">>> 08 AxesParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-    let mdx_fragment = "{
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004692[企业客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004692[企业客户] )
-}
-on 1,
-{
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001641[2024-05] )
-}
-on 0000";
-    let ast_node = AxesParser::new()
-        .parse(MdxLexer::new(mdx_fragment))
-        .unwrap();
-
-    println!("{:?}", ast_node);
-    println!("\n\n");
-}
-
-// SelectionMDXParser
-// #[test]
-fn test_parsing_mdx_09() {
-    println!("\n\n");
-    println!(">>> 09 SelectionMDXParser >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-    let mdx_fragment = "select
-{
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004718[Online Store], &600000000000006[22].&300000000004692[企业客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004685[VIP客户] ),
-( &600000000000007[333].&300000000004719[Retail Store], &600000000000006[22].&300000000004692[企业客户] )
-}
-on 1,
-{
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004570[中国], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004670[Credit Card], &600000000000001[rr].&300000000001641[2024-05] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001515[2024] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001548[2024-02] ),
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001641[2024-05] )
-}
-on 0
-from &500000000000001[多维立方体]
-where
-( &600000000000004[gggg].&300000000004571[上海], &600000000000005[11].&300000000004672[PayPal], &600000000000001[rr].&300000000001641[2024-05] )";
-    let ast_node = SelectionMDXParser::new()
-        .parse(MdxLexer::new(mdx_fragment))
-        .unwrap();
-
-    println!("{:?}", ast_node);
-    println!("\n\n");
-}
-
-// #[test]
-// fn test_parsing_mdx_demo_2_axposstr() {
-//     handle_stat(String::from("MDX"), mdx_demo_2_axposstr());
-// }
