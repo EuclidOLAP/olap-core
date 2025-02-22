08858c882db92b09581495c38acddf54d06febbe
Fir <czg@euclidolap.com>
2025-02-22 05:51:03 +0000

    ^ src/mdx_tokens.rs

--- src/mdx_tokens.rs
+++ src/mdx_tokens.rs
@@ -66,37 +66,6 @@ pub enum Token {
     #[regex("(?i)SECTIONS")]
     Sections,
 
-    // #[token("var")]
-    // KeywordVar,
-    // #[token("print")]
-    // KeywordPrint,
-
-    // #[regex("[_a-zA-Z][_0-9a-zA-Z]*", |lex| lex.slice().to_string())]
-    // Identifier(String),
-    // #[regex("[1-9][0-9]*", |lex| lex.slice().parse())]
-    // Integer(i64),
-
-    // #[token("(")]
-    // LParen,
-    // #[token(")")]
-    // RParen,
-    // #[token("=")]
-    // Assign,
-    // #[token(";")]
-    // Semicolon,
-
-    // #[token("+")]
-    // OperatorAdd,
-    // #[token("-")]
-    // OperatorSub,
-    // #[token("*")]
-    // OperatorMul,
-    // #[token("/")]
-    // OperatorDiv,
-
-    // #[cfg(feature = "bit")]
-    // #[token("<<")]
-    // OperatorShl,
 }
 
 impl fmt::Display for Token {
