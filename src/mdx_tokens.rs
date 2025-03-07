2166ed500c4046f06e07a3f5f15b611d49d9383a
Fir <czg@euclidolap.com>
2025-03-07 10:40:20 +0000

    'Double' token was added to MDX grammar.

--- src/mdx_tokens.rs
+++ src/mdx_tokens.rs
@@ -1,10 +1,11 @@
 use logos::Logos;
 use std::fmt; // to implement the Display trait
-use std::num::ParseIntError;
+use std::num::{ParseFloatError, ParseIntError};
 
 #[derive(Default, Debug, Clone, PartialEq)]
 pub enum LexicalError {
     InvalidInteger(ParseIntError),
+    InvalidDouble(ParseFloatError),
     #[default]
     InvalidToken,
 }
@@ -15,10 +16,15 @@ impl From<ParseIntError> for LexicalError {
     }
 }
 
+impl From<ParseFloatError> for LexicalError {
+    fn from(err: ParseFloatError) -> Self {
+        LexicalError::InvalidDouble(err)
+    }
+}
+
 #[derive(Logos, Clone, Debug, PartialEq)]
 #[logos(skip r"[ \t\n\f]+", skip r"#.*\n?", skip r"--.*\n?", error = LexicalError)]
 pub enum Token {
-
     #[regex("(?i)With")]
     With,
     #[regex("(?i)Member")]
@@ -72,6 +78,8 @@ pub enum Token {
 
     #[regex("[0-9]+", |lex| lex.slice().parse())]
     Integer(u64),
+    #[regex(r"[0-9]+\.[0-9]+", |lex| lex.slice().parse())]
+    Double(f64),
 
     #[regex("(?i)COLUMNS")]
     Columns,
@@ -83,7 +91,6 @@ pub enum Token {
     Chapters,
     #[regex("(?i)SECTIONS")]
     Sections,
-
 }
 
 impl fmt::Display for Token {
