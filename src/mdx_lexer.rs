a91eea6a47bdb6ecf33fa5e138e6994614a90c88
Fir <czg@euclidolap.com>
2025-05-31 03:11:01 +0000

    Formatted a few code in src/mdx_lexer.rs.

--- src/mdx_lexer.rs
+++ src/mdx_lexer.rs
@@ -12,9 +12,7 @@ pub struct Lexer<'input> {
 impl<'input> Lexer<'input> {
     pub fn new(input: &'input str) -> Self {
         // the Token::lexer() method is provided by the Logos trait
-        Self {
-            token_stream: Token::lexer(input).spanned(),
-        }
+        Self { token_stream: Token::lexer(input).spanned() }
     }
 }
 
