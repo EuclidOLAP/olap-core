7926032831507107dde08241b3950e01eb6edb2e
Fir <czg@euclidolap.com>
2025-06-26 00:33:15 +0000

    New garmmar have been developing. (SegsObj: AstSegsObj)

--- src/core/ast.rs
+++ src/core/ast.rs
@@ -17,3 +17,18 @@ pub enum AstSeg {
     SetFunc(()),
     ExpFunc(()),
 }
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct AstSegsObj {
+    segs: Vec<AstSeg>,
+}
+
+impl AstSegsObj {
+    pub fn new(seg: AstSeg) -> Self {
+        Self { segs: vec![seg] }
+    }
+
+    pub fn append(&mut self, seg: AstSeg) {
+        self.segs.push(seg)
+    }
+}
